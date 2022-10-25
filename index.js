a450e62898073c0f845b047f1d5ef6144fedcf63
Hiba <eng.lilil92@gmail.com>
2022-10-25 20:51:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+//const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+
+function findMatching(arr , str) {
+    return  arr.filter((driver)=>{
+        return driver.toLowerCase() === str.toLowerCase();  
+    })
+}
+
+function fuzzyMatch(arr , letter) {
+    return arr.filter((driver)=>{
+        return driver.startsWith(letter);
+    })
+}
+
+function matchName(arr , str) {
+    return arr.filter((driver)=>{
+      if(driver.name === str){
+        return driver.name;
+      }
+    })
+}
